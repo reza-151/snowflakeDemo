c0161ab15256c34903595a7269525ddc8e0f14d0
reza-151 <reza.soltani@one51.com.au>
2024-10-01 22:59:16 +1000

    One51 Composer

---
+++ models/example/HSTG_HSTG_TCS_CashRegTransDtl_snapshot.sql
@@ -0,0 +1,17 @@
+
+    {% snapshot HSTG_HSTG_TCS_CashRegTransDtl_snapshot %}
+
+    {{
+        config(
+            target_schema='HSTG',
+            unique_key='OMD_HASH_BUSINESS_KEY',
+            strategy='check',
+            check_cols='all',
+            invalidate_hard_deletes=True,
+        )
+    }}
+
+    select * from {{ source('STG', 'STG_TCS_CashRegTransDtl') }}
+
+    {% endsnapshot %}
+
