6f7a37e19c261902f7b73964ee63f29bb275f763
reza-151 <reza.soltani@one51.com.au>
2024-10-09 10:00:33 +1100

    One51 Composer

---
+++ models/example/STG_HRMS_departments.sql
@@ -0,0 +1,15 @@
+
+    /**STG_HRMS_departments**/
+    {{
+          config(
+            materialized = 'table',
+        )
+    }}
+                SELECT * FROM (
+                    SELECT 
+                        CAST("DEPARTMENT_ID" AS NUMBER(38,0)) AS "DEPARTMENT_ID"
+                        ,CAST("DEPARTMENT_NAME" AS TEXT(100) ) AS "DEPARTMENT_NAME"
+                        ,CAST("LOCATION_ID" AS NUMBER(38,0)) AS "LOCATION_ID"
+                    FROM {{ref('LND_HRMS_departments')}}
+                    ) T 
+
