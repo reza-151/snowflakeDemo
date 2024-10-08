64974e156d5a02240075a787e18c041171a5b748
reza-151 <reza.soltani@one51.com.au>
2024-10-09 10:52:04 +1100

    One51 Composer

---
+++ models/example/STG_HRMS_regions.sql
@@ -0,0 +1,13 @@
+    /**STG_HRMS_regions**/
+    {{
+          config(
+            materialized = 'table',
+        )
+    }}
+                SELECT * FROM (
+                    SELECT 
+                        CAST("REGION_ID" AS NUMBER(38,0)) AS "REGION_ID"
+                        ,CAST("REGION_NAME" AS TEXT(100) ) AS "REGION_NAME"
+                    FROM {{source('LND','LND_HRMS_regions')}}
+                    ) T 
+
